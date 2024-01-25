d5a7878f0e67da9bb6b40ce77fcda568f408454a
bayurifkialghifari <bayurifkialgh@gmail.com>
2024-01-25 14:45:12 +0700

    add middleware api key

--- resources/views/livewire/cms/management/access-control.blade.php
+++ resources/views/livewire/cms/management/access-control.blade.php
@@ -13,7 +13,7 @@
                     <div class="mb-3">
                         <label class="form-label fw-bold">API KEY</label>
                         <p x-ref="apikey">{{ $apikey }}</p>
-                        <button class="btn btn-primary" x-on:click="navigator.clipboard.writeText('{{ $apikey }}');alert('Copied!')">
+                        <button class="btn btn-primary" x-on:click="navigator.clipboard.writeText($refs.apikey.innerText);">
                             <i class="align-middle" data-feather="copy"></i>
                         </button>
                     </div>
--- routes/api/v1.php
+++ routes/api/v1.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::group([
     'as' => 'api.v1',
+    'middleware' => ['api', 'auth.api-key'],
 ], function () {
     Route::get('/tv', [App\Http\Controllers\Api\V1\TvController::class, 'get'])->name('tv');
 });
