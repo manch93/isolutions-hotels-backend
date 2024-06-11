d7c05ef238bd8c3989200d6ee93719b0c5a66243
bayurifkialghifari <bayurifkialgh@gmail.com>
2024-06-11 11:06:12 +0700

    up max upload

--- config/livewire.php
+++ config/livewire.php
@@ -66,7 +66,7 @@ return [
     'temporary_file_upload' => [
         'disk' => null,        // Example: 'local', 's3'              | Default: 'default'
         // 100MB maximum file upload size
-        'rules' => ['file', 'max:102400'],       // Example: ['file', 'mimes:png,jpg']  | Default: ['required', 'file', 'max:12288'] (12MB)
+        'rules' => ['file', 'max:1024000'],       // Example: ['file', 'mimes:png,jpg']  | Default: ['required', 'file', 'max:12288'] (12MB)
         'directory' => null,   // Example: 'tmp'                      | Default: 'livewire-tmp'
         'middleware' => null,  // Example: 'throttle:5,1'             | Default: 'throttle:60,1'
         'preview_mimes' => [   // Supported file types for temporary pre-signed file URLs...
