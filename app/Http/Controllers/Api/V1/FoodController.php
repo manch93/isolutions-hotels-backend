da3856a412f576badd4aa949987253367e4345d8
fajarxfce <nurul.10120014@mahasiswa.unikom.ac.id>
2025-07-06 07:07:00 +0700

    add food pagination

--- app/Http/Controllers/Api/V1/FoodController.php
+++ app/Http/Controllers/Api/V1/FoodController.php
@@ -5,9 +5,12 @@ namespace App\Http\Controllers\Api\V1;
 use App\Models\Food;
 use App\Models\FoodCategory;
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Traits\WithGetFilterDataApi;
 
 class FoodController extends Controller
 {
+    use WithGetFilterDataApi;
     public function category() {
         return $this->respondWithSuccess(FoodCategory::where('hotel_id', $this->getHotel())->get());
     }
@@ -16,7 +19,19 @@ class FoodController extends Controller
         return $this->respondWithSuccess(Food::where('food_category_id', $category)->get());
     }
 
-    public function get() {
-        return $this->respondWithSuccess(Food::where('hotel_id', $this->getHotel())->get());
+    public function get(Request $request) {
+        $data = $this->getDataWithFilter(
+            model: new Food,
+            searchBy: [
+                'name',
+                'description',
+            ],
+            orderBy: $request?->orderBy ?? 'id',
+            order: $request?->order ?? 'asc',
+            paginate: $request?->paginate ?? 10,
+            searchBySpecific: $request?->searchBySpecific ?? '',
+            s: $request?->search ?? '',
+        );
+        return $this->respondWithSuccess($data);
     }
 }
